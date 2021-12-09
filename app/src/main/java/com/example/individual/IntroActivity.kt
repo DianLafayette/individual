f07bdd4f579942ba08e24244d3065803b2edf29f
Notnik_kg <ulanovadanaya@gmail.com>
2021-12-09 14:11:02 +0600

    14:10 09.12.21

--- app/src/main/java/com/example/individual/IntroActivity.kt
+++ app/src/main/java/com/example/individual/IntroActivity.kt
@@ -2,10 +2,23 @@ package com.example.individual
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.individual.databinding.ActivityMainBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+
     }
 }
+
