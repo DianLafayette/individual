67d48dd8a77c1e3807554d3a1544aa99d5b3c0ff
Notnik_kg <ulanovadanaya@gmail.com>
2021-12-10 17:35:56 +0600

    17:33 10.12.21
    новый экран c главным меню и тремя переходами готов. Отличется от оригинаа поскольку нижний навигейшн бар перекрывает категории. Также упрощены кнопки поскольку в противном случае текст в них не вместится (почему то он только больими буквами пишется в кнопках)
    также добавлены новые цвета и стринги

--- app/src/main/java/com/example/individual/MainmenuActivity.kt
+++ app/src/main/java/com/example/individual/MainmenuActivity.kt
@@ -1,11 +1,37 @@
 package com.example.individual
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.individual.databinding.ActivityCardBinding
+import com.example.individual.databinding.ActivityLorrBinding
+import com.example.individual.databinding.ActivityMainmenuBinding
+import com.example.individual.databinding.ActivityPromoBinding
 
 class MainmenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainmenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainmenu)
+
+
+        binding = ActivityMainmenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goMainmenu.setOnClickListener {
+            startActivity(Intent(this, MainmenuActivity::class.java))
+        }
+
+        binding.goPromo.setOnClickListener {
+            startActivity(Intent(this, ActivityPromoBinding::class.java))
+        }
+
+
+        binding.goCard.setOnClickListener {
+            startActivity(Intent(this, ActivityCardBinding::class.java))
+        }
+
+
+
     }
 }
